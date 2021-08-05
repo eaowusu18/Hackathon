edfea3e2be1ca22a7db6150b70f12b99f8b66bb3
Edward Owusu <eaowusu18@gmail.com>
2021-08-05 12:24:23 -0500

    Final

--- hack17health/src/App.js
+++ hack17health/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import Profile from './components/ProfileForm.js';
+import Paper  from '@material-ui/core/Paper';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Paper>
+      <Profile />
+    </Paper>
+      
   );
 }
 
--- hack17health/src/components/ProfileForm.js
+++ hack17health/src/components/ProfileForm.js
@@ -1,22 +1,102 @@
-import React from "react";
+import React, {useState} from "react";
+import '../css/App.css'
+import {FormGroup, FormLabel, Input, FormControlLabel, Checkbox} from '@material-ui/core';
 
 
 
 export default function Profile() {
+
+    const useLoginInput = (initialValue) => {
+        const [value, setValue] = useState(initialValue);
+    
+        const handleChange = e => {
+            setValue(e.target.value);
+        }
+        return { value, onChange: handleChange }
+    }
+
+    const [checkedState, setCheckedState] = useState(
+        new Array(8).fill(false)
+    );
+
+  
+
+  
+    
+    const fName = useLoginInput('');
+    const lName = useLoginInput('');
+    const userAge = useLoginInput('');
+    
+    const HandleLogin = () => {
+        console.log(fName);
+        console.log(lName);
+        
+    }
+
     return (
         <form>
-            <input id="fName" name="fName" placeholder="First Name" />
-            <input id="lName" name="lName" placeholder="Last Name"/>
-            <input type="number" id="userAge" name="userAge" min="13" max="100"/>
-            <input type="checkbox" id="dietPref" value="Low Sodium"/>
-            <input type="checkbox" id="dietPref" value="Low Fat"/>
-            <input type="checkbox" id="dietPref" value="Low Sugar"/>
-            <input type="checkbox" id="dietPref" value="Vegan"/>
-            <input type="checkbox" id="dietPref" value="Vegetarian"/>
-            <input type="checkbox" id="dietPref" value="Low Carb"/>
-            <input type="checkbox" id="dietPref" value="High protein"/>
-            <input type="checkbox" id="dietPref" value="Pescetarian"/>
-            <input type="checkbox" id="dietPref" value="Low Cholesterol"/>
+            <FormLabel for="fName">Enter First Name</FormLabel>
+            <Input id="fName" {...fName}  name="fName" placeholder="First Name" />
+            <br />
+            <FormLabel for="lName">Enter Last Name</FormLabel>
+            <Input id="lName" {...lName} name="lName" placeholder="Last Name"/>
+            <br />
+            <FormLabel for="userAge">Enter Age</FormLabel>
+            <Input type="number" id="userAge" {...userAge} name="userAge" min="13" max="100"/>
+            <br />
+            <FormControlLabel
+        control={
+          <Checkbox
+            name="Low Fat"
+            color="primary"
+          />
+        }
+        label="Low Fat"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            name="Low Fat"
+            color="primary"
+          />
+        }
+        label="Low Sugar"
+      /><FormControlLabel
+      control={
+        <Checkbox
+          name="Low Fat"
+          color="primary"
+        />
+      }
+      label="Low Carb"
+    /><FormControlLabel
+    control={
+      <Checkbox
+        name="Low Fat"
+        color="primary"
+      />
+    }
+    label="High Protein"
+  /><FormControlLabel
+  control={
+    <Checkbox
+      name="Low Fat"
+      color="primary"
+    />
+  }
+  label="Vegetarian"
+/><FormControlLabel
+        control={
+          <Checkbox
+            name="Low Fat"
+            color="primary"
+          />
+        }
+        label="Vegan"
+      />
+      <a href="C:\Users\eaowu\Hackathon\hack17health\src\meal.html">
+      <button>Meal</button>
+      </a>
         </form>
 
     );
